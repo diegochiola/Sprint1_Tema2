a72ba36a7de4fa7263cead54ac2e36f23de7556a
diega <diegochiola@gmail.com>
2023-12-14 09:23:33 +0100

    Agregado de comentarios

--- Ejercicio4_php.php
+++ Ejercicio4_php.php
@@ -4,15 +4,12 @@
 $numero = (int) 0;
 $cuantoEnCuanto = (int) 0;
 
-
-
 function contar(int $numero, int $cuantoEnCuanto): int{
   for ($i=0; $i <= $numero ; $i += $cuantoEnCuanto) { 
     echo $i . "</br>";
   }
 }
-
+//llamado de funcion y prueba 
 contar(20, 5);
-
 
 ?>
