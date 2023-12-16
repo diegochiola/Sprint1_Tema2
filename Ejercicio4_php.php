84666cd7ac29dba6c398c17b754d0ea526149307
diega <diegochiola@gmail.com>
2023-12-16 09:58:41 +0100

    Se corrigio el ejercicio agregando ekl parametro de cuanto en cuanto por defecto

--- Ejercicio4_php.php
+++ Ejercicio4_php.php
@@ -1,15 +1,15 @@
 <?php
 //Ejerccicio 4
 //funcion contar
-$numero = (int) 0;
-$cuantoEnCuanto = (int) 0;
+$numero = 0;
+$cuantoEnCuanto = 0;
 
-function contar(int $numero, int $cuantoEnCuanto): int{
+function contar(int $numero, int $cuantoEnCuanto = 2): int{ //se agrego el parametro por defecto
   for ($i=0; $i <= $numero ; $i += $cuantoEnCuanto) { 
     echo $i . "</br>";
   }
 }
 //llamado de funcion y prueba 
-contar(20, 5);
+contar(20,);
 
 ?>
